ad75a31d4e5c38a811228d250367fdfb27326ec6
cameronbourke <cameron2295@gmail.com>
2015-02-16 08:54:54 +1100

    Filter Searchers
    Signed-off-by: cameronbourke <cameron2295@gmail.com>

--- app/assets/javascripts/cfapp.js
+++ app/assets/javascripts/cfapp.js
@@ -28,10 +28,22 @@ angular.module('CoderFactory', ['ngAnimate'])
   courseList.showPtCourses = true;
   courseList.showFtCourses = true;
 
+  courseList.webDesign     = true;
+  courseList.ror           = true;
+  courseList.wordpress     = true;
+  courseList.tech          = true;
+  courseList.syd           = true;
+  courseList.mel           = true;
+  courseList.bns           = true;
+  courseList.ade           = true;
+  courseList.perth         = true;
+
   // methods
   courseList.toggleWorkshops = toggleWorkshops;
   courseList.togglePtCourses = togglePtCourses;
   courseList.toggleFtCourses = toggleFtCourses;
+  courseList.slcIntChanged   = slcIntChanged;
+  courseList.slcLocChanged   = slcLocChanged;
 
   function toggleWorkshops() {
     courseList.showWorkshops = !courseList.showWorkshops;
@@ -43,6 +55,74 @@ angular.module('CoderFactory', ['ngAnimate'])
 
   function toggleFtCourses() {
     courseList.showFtCourses = !courseList.showFtCourses;
+  }
+
+  function slcIntChanged(newVal) {
+    console.log(newVal);
+    courseList.showWorkshops = true;
+    courseList.showPtCourses = true;
+    courseList.showFtCourses = true;
+    if(newVal === 'web design') {
+      courseList.webDesign = true;
+      courseList.ror = false;
+      courseList.wordpress = false;
+      courseList.tech = false;
+    } else if(newVal === 'wordpress') {
+      courseList.webDesign = false;
+      courseList.ror = false;
+      courseList.wordpress = true;
+      courseList.tech = false;
+    } else if(newVal === 'technology') {
+      courseList.webDesign = false;
+      courseList.ror = false;
+      courseList.wordpress = false;
+      courseList.tech = true;
+    } else if(newVal === 'ruby on rails') {
+      courseList.webDesign = false;
+      courseList.ror = true;
+      courseList.wordpress = false;
+      courseList.tech = false;
+    } else if(newVal === 'how to code') {
+    courseList.webDesign = true;
+    courseList.ror = true;
+    courseList.wordpress = true;
+    courseList.tech = true;
+    }
+  }
+
+  function slcLocChanged(newVal) {
+    console.log(newVal);
+    if(newVal === 'Melbourne') {
+      courseList.syd           = false;
+      courseList.mel           = true;
+      courseList.bns           = false;
+      courseList.ade           = false;
+      courseList.perth         = false;
+    } else if(newVal === 'Sydney') {
+      courseList.syd           = true;
+      courseList.mel           = false;
+      courseList.bns           = false;
+      courseList.ade           = false;
+      courseList.perth         = false;
+    } else if(newVal === 'Adelaide') {
+      courseList.syd           = false;
+      courseList.mel           = false;
+      courseList.bns           = false;
+      courseList.ade           = true;
+      courseList.perth         = false;
+    } else if(newVal === 'Perth') {
+      courseList.syd           = false;
+      courseList.mel           = false;
+      courseList.bns           = false;
+      courseList.ade           = false;
+      courseList.perth         = true;
+    } else if(newVal === 'Brisbane') {
+      courseList.syd           = false;
+      courseList.mel           = false;
+      courseList.bns           = true;
+      courseList.ade           = false;
+      courseList.perth         = false;
+    }
   }
 
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,31 @@
 class ProductsController < ApplicationController
-  
+
   before_filter do
     if params[:category_id]
       @product_category = Shoppe::ProductCategory.where(:permalink => params[:category_id]).first!
     end
     if @product_category && params[:product_id]
-      @product = @product_category.products.where(:permalink => params[:product_id]).active.first!      
+      @product = @product_category.products.where(:permalink => params[:product_id]).active.first!
     end
   end
-  
+
   def index
     @products = @product_category.products.includes(:product_category, :variants).root.active
   end
-  
+
   def filter
     @products = Shoppe::Product.active.with_attributes(params[:key].to_s, params[:value].to_s)
   end
-  
+
   def categories
-    @product_categories = Shoppe::ProductCategory.ordered
+    @products = Shoppe::Product.ordered
   end
   
   def show
     @attributes = @product.product_attributes.public.to_a
     @product_variants = @product.variants
   end
-  
+
   def add_to_basket
     product_to_order = params[:variant] ? @product.variants.find(params[:variant].to_i) : @product
     current_order.order_items.add_item(product_to_order, params[:quantity].blank? ? 1 : params[:quantity].to_i)
@@ -39,5 +39,5 @@ class ProductsController < ApplicationController
       wants.json { render :json => {:error => 'NotEnoughStock', :available_stock => e.available_stock}}
     end
   end
-  
+
 end
